56f5403ea5b0377c802195d50bc3cd02d9631b8b
Jeff Bailey <bailey27@illinois.edu>
2013-09-04 23:25:53 -0500

    Importing works short of parsing

--- FFR/src/com/example/fantasyfootballrankings/LeagueImports/ESPNImport.java
+++ FFR/src/com/example/fantasyfootballrankings/LeagueImports/ESPNImport.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import jeff.isawesome.fantasyfootballrankings.R;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -28,9 +30,317 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.example.fantasyfootballrankings.ClassFiles.HighLevel;
+import com.example.fantasyfootballrankings.ClassFiles.StorageClasses.PlayerObject;
+import com.example.fantasyfootballrankings.ClassFiles.StorageClasses.Storage;
+import com.example.fantasyfootballrankings.ClassFiles.StorageClasses.TeamAnalysis;
+
+import FileIO.WriteToFile;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class ESPNImport 
 {
+	public String url;
+	public String username;
+	public String password;
+	public Document doc;
+	public Context cont;
+	
+	/**
+	 * Gets the process started with a trial query
+	 * @param urlOrig
+	 * @param c
+	 * @throws IOException
+	 */
+	public void handleESPNParsing(String urlOrig, final Context c) throws IOException
+	{
+		cont = c;
+		url = urlOrig;
+		((Activity) cont).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		GetTestDoc task1 = this.new GetTestDoc((Activity)cont, this);
+		task1.execute(url);
+	}
+	
+	/**
+	 * Gets the initial doc from the URL to see if any more work is necessary
+	 * @author Jeff
+	 *
+	 */
+	public class GetTestDoc extends AsyncTask<Object, String, Document> 
+	{
+		Activity act;
+		ESPNImport obj;
+	    public GetTestDoc(Activity activity, ESPNImport espnImport) 
+	    {
+	        act = activity;
+	        obj = espnImport;
+	    }
 
+		@Override
+		protected void onPreExecute(){ 
+		   super.onPreExecute();   
+		}
+
+		@Override
+		protected void onPostExecute(Document result){
+			act.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+		   super.onPostExecute(result);
+		   obj.handleTest(result);
+		}
+
+	    @Override
+	    protected Document doInBackground(Object... data) 
+	    {
+	    	String url = (String)data[0];
+	    	
+			try {
+				return Jsoup.connect(url).timeout(0).get();
+			} catch (IOException e) {
+				return null;
+			}
+	    }
+
+	  }
+	
+	/**
+	 * Uses the new test document to see if someone needs to sign in at all
+	 * @param test
+	 */
+	public void handleTest(Document test)
+	{
+		if(isSignIn(test))
+		{
+			handleSignInNeed(test);
+		}
+		else if(!isRosters(test))
+		{
+			final Dialog popUp = new Dialog(cont, R.style.RoundCornersFull);
+		    popUp.requestWindowFeature(Window.FEATURE_NO_TITLE);       
+			popUp.setContentView(R.layout.tweet_popup);
+			WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+		    lp.copyFrom(popUp.getWindow().getAttributes());
+		    lp.width = WindowManager.LayoutParams.FILL_PARENT;
+		    popUp.getWindow().setAttributes(lp);
+		    popUp.show(); 
+		    TextView textView = (TextView)popUp.findViewById(R.id.tweet_field);
+		    textView.setText("   The league ID you input was invalid.\n\n");
+		    Button close = (Button)popUp.findViewById(R.id.tweet_popup_close);
+		    close.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					popUp.dismiss();
+					return;
+				}
+		    });
+		}  
+		else
+		{
+			doc = test;
+			handleParsing();
+		}
+		
+	}
+	
+	/**
+	 * Handles signing in if need be
+	 * @param test
+	 */
+	public void handleSignInNeed(Document test)
+	{
+		if(!isCredentialsSet(cont))
+		{
+			setLogIn();
+		}
+		else
+		{
+			readUnPw(cont);
+			((Activity) cont).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+			GetLogInFirst task = this.new GetLogInFirst((Activity)cont, this);
+			task.execute();
+		}
+	}
+	
+	/**
+	 * Gets the initial doc from the URL to see if any more work is necessary
+	 * @author Jeff
+	 *
+	 */
+	public class GetLogInFirst extends AsyncTask<Object, String, Document> 
+	{
+		Activity act;
+		ESPNImport obj;
+	    public GetLogInFirst(Activity activity, ESPNImport espnImport) 
+	    {
+	        act = activity;
+	        obj = espnImport;
+	    }
+
+		@Override
+		protected void onPreExecute(){ 
+		   super.onPreExecute();   
+		}
+
+		@Override
+		protected void onPostExecute(Document result){
+			act.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+		   super.onPostExecute(result);
+		   obj.handleFirstLogIn(result);
+		}
+
+	    @Override
+	    protected Document doInBackground(Object... data) 
+	    {
+	    	try {
+				Document test = parseESPNPassWord(url, username, password);
+				return test;
+			} catch (IOException e) {
+				return null;
+			}
+
+	    }
+
+	  }
+	
+	/**
+	 * Handles the logic of the first sign in attempt
+	 * @param test
+	 */
+	public void handleFirstLogIn(Document test)
+	{
+		if(isSignIn(test))
+		{
+			Toast.makeText(cont, "Log in failed with the stored information", Toast.LENGTH_SHORT).show();
+			setLogIn();
+		}
+		else
+		{
+			doc = test;
+			handleParsing();
+		}
+	}
+	
+	/**
+	 * Handles the logging in
+	 */
+	public void setLogIn()
+	{
+		final Dialog popUp = new Dialog(cont, R.style.RoundCornersFull);
+	    popUp.requestWindowFeature(Window.FEATURE_NO_TITLE);       
+		popUp.setContentView(R.layout.espn_unpw);
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+	    lp.copyFrom(popUp.getWindow().getAttributes());
+	    lp.width = WindowManager.LayoutParams.FILL_PARENT;
+	    popUp.getWindow().setAttributes(lp);
+	    popUp.show(); 
+	    Button close = (Button)popUp.findViewById(R.id.espn_unpw_close);
+	    close.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				popUp.dismiss();
+				return;
+			}
+	    });
+	    final EditText unField = (EditText)popUp.findViewById(R.id.username_input);
+	    final EditText pwField = (EditText)popUp.findViewById(R.id.password_input);
+	    Button submit = (Button)popUp.findViewById(R.id.espn_unpw_submit);
+	    final ESPNImport obj = this;
+	    submit.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				HandleLogIn task = obj.new HandleLogIn((Activity)cont, obj, popUp);
+				String un = unField.getText().toString();
+				String pw = pwField.getText().toString();
+				if(un.length() > 0 && pw.length() > 0)
+				{
+					task.execute(un, pw);
+					popUp.dismiss();
+				}
+				else
+				{
+					Toast.makeText(cont, "Please enter a username and a password", Toast.LENGTH_SHORT).show();
+				}
+			}
+	    });
+	}
+	
+	/**
+	 * Gets the initial doc from the URL to see if any more work is necessary
+	 * @author Jeff
+	 *
+	 */
+	public class HandleLogIn extends AsyncTask<Object, String, Document> 
+	{
+		Activity act;
+		ESPNImport obj;
+		Dialog dialog;
+	    public HandleLogIn(Activity activity, ESPNImport espnImport, Dialog popUp) 
+	    {
+	        act = activity;
+	        obj = espnImport;
+	        dialog = popUp;
+	    }
+
+		@Override
+		protected void onPreExecute(){ 
+		   super.onPreExecute();   
+		}
+
+		@Override
+		protected void onPostExecute(Document result){
+			act.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+		   super.onPostExecute(result);
+		   if(result == null)
+		   {
+			   Toast.makeText(cont, "Invalid username/password", Toast.LENGTH_SHORT).show();
+			   obj.setLogIn();
+		   }
+		   else
+		   {
+			   obj.handleParsing();
+		   }
+		}
+
+	    @Override
+	    protected Document doInBackground(Object... data) 
+	    {
+	    	String un = (String)data[0];
+	    	String pw = (String)data[1];
+			try {
+				Document testSignIn = parseESPNPassWord(url, un, pw);
+				if(isRosters(testSignIn))
+				{
+					obj.doc = testSignIn;
+					obj.username = un;
+					obj.password = pw;
+					storeUnPw(cont);
+					return obj.doc;
+				}
+			} catch (IOException e) {
+				System.out.println(e.toString());
+			}
+			return null;
+	    }
+
+	  }
+	
+	public void handleParsing()
+	{
+
+	}
 	
 	/**
 	 * Returns the html of the document when it for sure needs a password.
@@ -46,6 +356,9 @@ public class ESPNImport
 			    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
 			    .method(Method.POST)
 			    .execute();
+		System.out.println(url);
+		System.out.println(username);
+		System.out.println(password);
 			Document doc2 = Jsoup.connect(url)
 			    .cookies(res.cookies()) 
 			    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
@@ -70,6 +383,59 @@ public class ESPNImport
 		}
 		return false;
 	}
+	
+	/**
+	 * Checks if the document is currently the league rosters page
+	 * @param doc
+	 * @return
+	 */
+	public boolean isRosters(Document doc)
+	{
+		Elements elements = doc.select("title");
+		for(Element elem : elements)
+		{
+			if(elem.text().contains("League Rosters"))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns if the username/password are stored
+	 */
+	public boolean isCredentialsSet(Context cont)
+	{
+		SharedPreferences prefs = cont.getSharedPreferences("FFR", 0); 
+		return prefs.getBoolean("ESPN Credentials Stored", false);
+	}
+	
+	/**
+	 * Reads the stored username and password from file
+	 * @param cont
+	 */
+	public void readUnPw(Context cont)
+	{
+		SharedPreferences prefs = cont.getSharedPreferences("FFR", 0); 
+		username = prefs.getString("ESPN Username", "Not Set");
+		password = prefs.getString("ESPN Password", "Not Set");
+	}
+	
+	/**
+	 * Stores the username and password to avoid unnecessary hassle later
+	 * @param cont
+	 */
+	public void storeUnPw(Context cont)
+	{
+		SharedPreferences.Editor editor = cont.getSharedPreferences("FFR", 0).edit();
+		editor.putString("ESPN Username", username);
+		editor.putString("ESPN Password", password);
+		editor.putBoolean("ESPN Credentials Stored", true);
+		editor.commit();
+	}
+	
+	
 
 }
  
--- FFR/src/com/example/fantasyfootballrankings/Pages/ImportLeague.java
+++ FFR/src/com/example/fantasyfootballrankings/Pages/ImportLeague.java
@@ -1,6 +1,10 @@
 package com.example.fantasyfootballrankings.Pages;
 
+import java.io.IOException;
+
+import com.example.fantasyfootballrankings.ClassFiles.ManageInput;
 import com.example.fantasyfootballrankings.ClassFiles.StorageClasses.Storage;
+import com.example.fantasyfootballrankings.LeagueImports.ESPNImport;
 
 import jeff.isawesome.fantasyfootballrankings.R;
 import jeff.isawesome.fantasyfootballrankings.R.layout;
@@ -9,11 +13,22 @@ import FileIO.ReadFromFile;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 /**
  * Handles the importing of leagues
  * @author Jeff
@@ -22,7 +37,9 @@ import android.view.MenuItem;
 public class ImportLeague extends Activity {
 	public Context cont;
 	public static Storage holder = new Storage(null);
-	
+	public static LinearLayout ll;
+	SharedPreferences prefs;
+
 	/**
 	 * Sets up the layout of the activity
 	 */
@@ -34,7 +51,7 @@ public class ImportLeague extends Activity {
 		ActionBar ab = getActionBar();
 		ab.setDisplayShowHomeEnabled(false);
 		ab.setDisplayShowTitleEnabled(false);
-		SharedPreferences prefs = cont.getSharedPreferences("FFR", 0); 
+		prefs = cont.getSharedPreferences("FFR", 0); 
 		if(holder.players.size() < 10 || prefs.getBoolean("Home Update Import", false))
 		{
 			SharedPreferences.Editor editor = cont.getSharedPreferences("FFR", 0).edit();
@@ -46,6 +63,8 @@ public class ImportLeague extends Activity {
 	    	}
 
 		} 
+		ll = (LinearLayout)findViewById(R.id.import_base);
+		handleLayoutInit();
 	}
 
 	/**
@@ -86,9 +105,133 @@ public class ImportLeague extends Activity {
 		        Intent news_intent = new Intent(cont, News.class);
 		        cont.startActivity(news_intent);		
 				return true;
+			case R.id.importa_league:
+				handleImportInit();
+				return true;
 			default:
 				return super.onOptionsItemSelected(item);
 		}
 	}
 
+	/**
+	 * Handles the displaying of the layouts
+	 */
+	public void handleLayoutInit()
+	{
+		if(prefs.getInt("Number of Leagues Imported", 0) == 0)
+		{
+			setNoContentLayout();
+		}
+		else
+		{
+			//TODO: LOAD THE CONTENT
+		}
+	}
+
+	/**
+	 * If there is no content to be loaded, this says that
+	 */ 
+	public void setNoContentLayout() 
+	{
+		View res = ((Activity) cont).getLayoutInflater().inflate(R.layout.import_none_imported, ll, false);
+		ll.addView(res);
+	}
+	
+	/**
+	 * Handles the initial importing of a league
+	 */
+	public void handleImportInit()
+	{
+		final Dialog popUp = new Dialog(cont, R.style.RoundCornersFull);
+	    popUp.requestWindowFeature(Window.FEATURE_NO_TITLE);       
+		popUp.setContentView(R.layout.import_service_decider);
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+	    lp.copyFrom(popUp.getWindow().getAttributes());
+	    lp.width = WindowManager.LayoutParams.FILL_PARENT;
+	    popUp.getWindow().setAttributes(lp);
+	    popUp.show(); 
+	    Button close = (Button)popUp.findViewById(R.id.import_decider_close);
+	    close.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				popUp.dismiss();
+				return;
+			}
+	    });
+	    final RadioButton espn = (RadioButton)popUp.findViewById(R.id.espn_import);
+	    Button submit = (Button)popUp.findViewById(R.id.import_decide_submit);
+	    submit.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(espn.isChecked())
+				{
+					espnGetLeagueID();
+					popUp.dismiss();
+				}
+			}
+	    });
+	}
+	
+	/**
+	 * Puts up a pop up to get the ESPN league ID
+	 */
+	public void espnGetLeagueID()
+	{
+		final Dialog popUp = new Dialog(cont, R.style.RoundCornersFull);
+	    popUp.requestWindowFeature(Window.FEATURE_NO_TITLE);       
+		popUp.setContentView(R.layout.import_espn_league_id);
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+	    lp.copyFrom(popUp.getWindow().getAttributes());
+	    lp.width = WindowManager.LayoutParams.FILL_PARENT;
+	    popUp.getWindow().setAttributes(lp);
+	    popUp.show(); 
+	    Button close = (Button)popUp.findViewById(R.id.import_espnid_close);
+	    close.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				popUp.dismiss();
+				return;
+			}
+	    });
+	    Button back = (Button)popUp.findViewById(R.id.import_espnid_back);
+	    back.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				popUp.dismiss();
+				handleImportInit();
+				return;
+			}
+	    });
+	    final EditText id = (EditText)popUp.findViewById(R.id.league_id_espn_input);
+	    Button submit = (Button)popUp.findViewById(R.id.espn_id_input_submit);
+	    submit.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				String test = id.getText().toString();
+				if(test.length() > 1 && ManageInput.isInteger(test))
+				{
+					popUp.dismiss();
+					callESPNParsing(test);
+				}
+				else
+				{
+					Toast.makeText(cont, "Please enter a number", Toast.LENGTH_SHORT).show();
+				}
+			}
+	    });
+	}
+	
+	/**
+	 * Calls the espn parser
+	 * @param id
+	 */
+	public void callESPNParsing(String id)
+	{
+		ESPNImport espnImporter = new ESPNImport();
+		try {
+			espnImporter.handleESPNParsing("http://games.espn.go.com/ffl/leaguerosters?leagueId=" + id, cont);
+		} catch (IOException e) {
+			Toast.makeText(cont, "There was an error, do you have a valid internet connection?", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
